a114abd293aa7f4eec47190159f60a68df5d00af
yousoumar <oumaiyr@gmail.com>
2023-10-10 15:54:23 +0200

    Add project starter files

---
+++ movie/movie.py
@@ -0,0 +1,59 @@
+from ariadne import (
+    graphql_sync,
+    make_executable_schema,
+    load_schema_from_path,
+    ObjectType,
+    QueryType,
+    MutationType,
+)
+from ariadne.constants import PLAYGROUND_HTML
+from flask import Flask, make_response, request, jsonify
+
+import resolvers as r
+
+PORT = 3001
+HOST = "localhost"
+app = Flask(__name__)
+
+# todo create elements for Ariadne
+type_defs = load_schema_from_path("movie.graphql")
+query = QueryType()
+mutation = MutationType()
+movie = ObjectType("Movie")
+actor = ObjectType("Actor")
+
+mutation.set_field("update_movie_rate", r.update_movie_rate)
+query.set_field("movie_with_id", r.movie_with_id)
+movie.set_field("actors", r.resolve_actors_in_movie)
+
+schema = make_executable_schema(type_defs, movie, query, mutation, actor)
+
+
+# root message
+@app.route("/", methods=["GET"])
+def home():
+    return make_response(
+        "<h1 style='color:blue'>Welcome to the Movie service!</h1>", 200
+    )
+
+
+#####
+# graphql entry points
+
+
+@app.route("/graphql", methods=["GET"])
+def playground():
+    return PLAYGROUND_HTML, 200
+
+
+@app.route("/graphql", methods=["POST"])
+def graphql_server():
+    data = request.get_json()
+    success, result = graphql_sync(schema, data, context_value=None, debug=app.debug)
+    status_code = 200 if success else 400
+    return jsonify(result), status_code
+
+
+if __name__ == "__main__":
+    print("Server running in port %s" % (PORT))
+    app.run(host=HOST, port=PORT)
